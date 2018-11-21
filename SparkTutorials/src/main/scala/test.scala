29b08739d449a5521f4f52ece63cefab24e2c77e
itechseeker <itechseeker.com@gmail.com>
2018-11-21 08:02:12 +0700

    Full code for the tutorial: http://itechseeker.com/tutorials/apache-spark/lap-trinh-spark-voi-intellij-idea/chay-mot-so-chuong-trinh-co-ban/

--- SparkTutorials/src/main/scala/test.scala
+++ SparkTutorials/src/main/scala/test.scala
@@ -3,18 +3,22 @@ import org.apache.spark.sql.SparkSession
 
 object test {
   def main(args: Array[String]) {
-    // Specific the path of the file
-    val textFile = "src/main/resources/README.md"
-    // Create a Spark session
+
+    //Define Spark Session
     val spark = SparkSession
       .builder()
       .appName("Spark Test")
       .config("spark.master", "local")
       .getOrCreate();
 
+    //Set the Log file level
     spark.sparkContext.setLogLevel("WARN")
+
+    // Specific the path of the file
+    val textFile = "src/main/resources/README.md"
 
     //Make a new Dataset from the text of README.md
     val textData = spark.read.textFile(textFile)
+    //Print some result
     println("Number of items in the Dataset: ",textData.count())}
 }
