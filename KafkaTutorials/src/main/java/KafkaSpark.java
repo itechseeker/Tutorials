32261fb5598cba1b0255904e13a4ce13712cae47
itechseeker <itechseeker.com@gmail.com>
2018-12-11 11:49:53 +0700

    Example of Kafka Spark Integration

--- KafkaTutorials/src/main/java/KafkaSpark.java
+++ KafkaTutorials/src/main/java/KafkaSpark.java
@@ -11,106 +11,49 @@ import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
+
 import scala.Tuple2;
-
-
 import java.util.*;
-
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
 
 public class KafkaSpark {
 
-    static Map<String, Object> kafkaParams = new HashMap<String, Object>();
-
-
     public static void main(String[] args) throws InterruptedException, StreamingQueryException {
 
         //Set log level to warn
         Logger.getLogger("org").setLevel(Level.OFF);
 
-        // Create a local StreamingContext with two working thread and batch interval of 1 second
-        SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("Sampleapp");
+        // Create a local StreamingContext and batch interval of 10 second
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("Kafka Spark Integration");
         JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(10));
 
+        //Define Kafka parameter
+        Map<String, Object> kafkaParams = new HashMap<String, Object>();
         kafkaParams.put("bootstrap.servers", "localhost:9092");
         kafkaParams.put("key.deserializer", StringDeserializer.class);
         kafkaParams.put("value.deserializer", StringDeserializer.class);
         kafkaParams.put("group.id", "0");
-        kafkaParams.put("auto.offset.reset", "earliest"); // get message from begining
+        // Automatically reset the offset to the earliest offset
+        kafkaParams.put("auto.offset.reset", "earliest");
         kafkaParams.put("enable.auto.commit", false);
 
+        //Define a list of Kafka topic to subscribe
         Collection<String> topics = Arrays.asList("test");
 
-        final JavaInputDStream<ConsumerRecord<String, String>> stream =
-                KafkaUtils.createDirectStream(
-                        jssc,
-                        LocationStrategies.PreferConsistent(),
-                        ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams)
-                );
-
-        /*stream.mapToPair(
-                new PairFunction<ConsumerRecord<String, String>, String, String>() {
-                    public Tuple2<String, String> call(ConsumerRecord<String, String> record) throws Exception {
-                        System.out.println("record key : "+record.key()+" value is : "+record.value());
-                        return new Tuple2<String, String>(record.key(), record.value());
-                    }
-                });*/
-
-        /*
-
-        JavaPairDStream<String, String> jPairDStream =  stream.mapToPair(
-                (PairFunction<ConsumerRecord<String, String>, String, String>) record -> new Tuple2<>(record.key(), record.value()));
-
-        jPairDStream.foreachRDD(jPairRDD -> {
-            jPairRDD.foreach(rdd -> { System.out.println("key="+rdd._1()+" value="+rdd._2()); });
-        });
+        //Create an input data stream which consume message from Kafka topics
+        JavaInputDStream<ConsumerRecord<String, String>> stream;
+        stream = KafkaUtils.createDirectStream(jssc,LocationStrategies.PreferConsistent(),ConsumerStrategies.Subscribe(topics, kafkaParams));
 
 
-
-        // Read value of each message from Kafka and return it
-        JavaDStream<String> lines = stream.map(new Function<ConsumerRecord<String,String>, String>() {
-            @Override
-            public String call(ConsumerRecord<String, String> kafkaRecord) throws Exception {
-                return kafkaRecord.value();
-            }
-        });
-
-         // Break every message into words and return list of words
-        JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-            @Override
-            public Iterator<String> call(String line) throws Exception {
-                return Arrays.asList(line.split(" ")).iterator();
-            }
-        });
-
-        // Take every word and return Tuple with (word,1)
-        JavaPairDStream<String,Integer> wordMap = words.mapToPair(new PairFunction<String, String, Integer>() {
-            @Override
-            public Tuple2<String, Integer> call(String word) throws Exception {
-                return new Tuple2<>(word,1);
-            }
-        });
-
-        // Count occurance of each word
-        JavaPairDStream<String,Integer> wordCount = wordMap.reduceByKey(new Function2<Integer, Integer, Integer>() {
-            @Override
-            public Integer call(Integer first, Integer second) throws Exception {
-                return first+second;
-            }
-        });
-
-        */
-
-        // Read value of each message from Kafka and return it
+        // Read value of each message from Kafka
         JavaDStream<String> lines = stream.map((Function<ConsumerRecord<String, String>, String>) kafkaRecord -> kafkaRecord.value());
 
-        // Break every message into words and return list of words
+        // Split message into words
         JavaDStream<String> words = lines.flatMap((FlatMapFunction<String, String>) line -> Arrays.asList(line.split(" ")).iterator());
 
         // Take every word and return Tuple with (word,1)
@@ -122,8 +65,8 @@ public class KafkaSpark {
         //Print the word count
         wordCount.print();
 
-
-        jssc.start();// Start the computation
+        // Start the computation
+        jssc.start();
         jssc.awaitTermination();
     }
 
--- KafkaTutorials/src/main/java/TestClass.java
+++ KafkaTutorials/src/main/java/TestClass.java
@@ -1,43 +1,5 @@
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.streaming.Duration;
-import org.apache.spark.streaming.api.java.JavaPairInputDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
-
-
-import kafka.serializer.StringDecoder;
-import org.apache.spark.streaming.kafka010.KafkaUtils;
-
 public class TestClass {
     public static void main(String[] args) {
-        /*SparkConf conf = new SparkConf()
-                .setAppName("kafka-sandbox")
-                .setMaster("local[*]");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaStreamingContext ssc = new JavaStreamingContext(sc, new Duration(2000));
 
-        Set<String> topics = Collections.singleton("mytopic");
-        Map<String, String> kafkaParams = new HashMap<String, String>();
-        kafkaParams.put("metadata.broker.list", "localhost:9092");
-
-        JavaPairInputDStream<String, String> directKafkaStream;
-        directKafkaStream = KafkaUtils.createDirectStream(ssc, String.class, String.class, StringDecoder.class, StringDecoder.class, kafkaParams, topics);
-
-        KafkaUtils.createDirectStream(ssc, String.class, String.class,
-                StringDecoder.class, StringDecoder.class, kafkaParams, topics);
-
-        directKafkaStream.foreachRDD(rdd -> {
-            System.out.println("--- New RDD with " + rdd.partitions().size()
-                    + " partitions and " + rdd.count() + " records");
-            rdd.foreach(record -> System.out.println(record._2));
-        });
-
-        ssc.start();
-        ssc.awaitTermination();*/
     }
 }
