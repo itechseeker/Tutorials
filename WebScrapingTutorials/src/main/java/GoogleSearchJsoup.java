bd12fa280e5b46a995e3a7cc8a71d5d0276a36b3
itechseeker <itechseeker.com@gmail.com>
2018-12-25 13:29:36 +0700

    Complete GoogleSearchJsoup

--- WebScrapingTutorials/src/main/java/GoogleSearchJsoup.java
+++ WebScrapingTutorials/src/main/java/GoogleSearchJsoup.java
@@ -2,34 +2,126 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+//Object to store search result
+class SearchResult{
+    private String title, url,summary;
+    public SearchResult(String title, String url, String summary)
+    {
+        this.title=title;
+        this.url=url;
+        this.summary=summary;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+    public String getUrl()
+    {
+        return url;
+    }
+    public String getSummary()
+    {
+        return summary;
+    }
+}
+
 public class GoogleSearchJsoup {
 
-    //Find the user agent on https://www.whatismybrowser.com/detect/what-is-my-user-agent
-    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
+    //Define user agent
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
     // Declare a Document object
-    static Document document;
+    private static Document document;
 
 
-    public static void main(String[] args) throws Exception {
-        String searchKey="Big data";
-        String dateRange="d1"; //d1:past 24 hours, m1: past month, y1: past year
-        int numOfResult=10;
+    public static void main(String[] args) {
 
-        String googleQuery="https://google.com/search?q="+searchKey+"&num="+numOfResult+"&tbs=qdr:"+dateRange;
-        System.out.println("The query string: "+ googleQuery);
-        document = Jsoup.connect(googleQuery).userAgent(USER_AGENT).timeout(5000).get();
-
-        //Traverse the results
-        for (Element result : document.getElementsByClass("g")){
-
-            String title = result.getElementsByTag("h3").text();
-            String url = result.getElementsByTag("cite").text();
-            String summary = result.getElementsByClass("st").text();
-
-            System.out.println("\nThe title: "+title);
-            System.out.println("The url: "+url);
-            System.out.println("Summary: "+summary);
+        //Search the key word "Web scraping" for the past 24 hours. Show the most 5 relevance results
+        ArrayList<SearchResult> searchResults= googleSearch("Big data",5,"w",0);
+        System.out.println("\nThe most 5 relevance results: ");
+        for(SearchResult sr:searchResults)
+        {
+            System.out.println("\nThe title: "+sr.getTitle());
+            System.out.println("The url: "+sr.getUrl());
+            System.out.println("Summary: "+sr.getSummary());
         }
 
+        //Search the key word "Web scraping" for the past 24 hours. Show the most 5 recent results
+        searchResults= googleSearch("Big data",5,"w",1);
+        System.out.println("\nThe most 5 recent results: ");
+        for(SearchResult sr:searchResults)
+        {
+            System.out.println("\nThe title: "+sr.getTitle());
+            System.out.println("The url: "+sr.getUrl());
+            System.out.println("Summary: "+sr.getSummary());
+        }
+
+        //Search the key word "Web scraping" for all time. Show the top 5  results
+        searchResults= googleSearch("Big data",5);
+        System.out.println("\nThe top 5 results of all time: ");
+        for(SearchResult sr:searchResults)
+        {
+            System.out.println("The title: "+sr.getTitle());
+        }
+
+        //Search the key word "Web scraping" for all time. Show the default  results
+        searchResults= googleSearch("Big data");
+        System.out.println("\nThe default 10 results of all time: ");
+        for(SearchResult sr:searchResults)
+        {
+            System.out.println("The title: "+sr.getTitle());
+        }
+    }
+
+    /**
+     * Get the result from google search
+     * @param searchKey The keywords for searching
+     * @param numOfResult The number of results need to be displayed
+     * @param dateRange The date range of the result
+     * @param sort 0:sort by relevance, 1: sort by date
+     * @return
+     */
+    private static ArrayList<SearchResult> googleSearch(String searchKey,int numOfResult, String dateRange, int sort)
+    {
+        //Construct the search query
+        String googleQuery="https://google.com/search?q="+searchKey+"&num="+numOfResult+"&tbs=qdr:"+dateRange+",sbd:"+sort;
+        //Print out the search query
+        System.out.println("\nThe query string: "+ googleQuery);
+
+        //An ArrayList to store the search results
+        ArrayList<SearchResult> results=new ArrayList<SearchResult>();
+        try {
+            document = Jsoup.connect(googleQuery).userAgent(USER_AGENT).timeout(5000).get();
+            //Traverse the results
+            for (Element result : document.getElementsByClass("g")){
+
+                //Extract the title, url and summary of each result
+                String title = result.getElementsByTag("h3").text();
+                String url = result.getElementsByTag("a").attr("href");
+                String summary = result.getElementsByClass("st").text();
+
+                //Add each result to the ArrayList
+                results.add(new SearchResult(title,url,summary));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return  results;
+    }
+
+    //Use this method if we dont need to specify the date range and sort order
+    //(Since java doesnt support default value so we use the overloaded approach)
+    private static ArrayList<SearchResult> googleSearch(String searchKey,int numOfResult)
+    {
+        return  googleSearch(searchKey,numOfResult,"a",0);
+    }
+
+    private static ArrayList<SearchResult> googleSearch(String searchKey)
+    {
+        return  googleSearch(searchKey,10,"a",0);
     }
 }
